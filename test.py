1ddbe63ae161e89d2463e964157f1167c5312c58
kylegomez <kyledominic.gomez@gmail.com>
2018-02-13 13:58:44 +0800

    iperf implemented. single host pair for now

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+
 from random import sample, choice
 
 # generate sender/receiver array
@@ -7,7 +8,6 @@ receiver = sample(xrange(length), length)
 sender = []
 for each in range(0, length):
 	sender.append(choice([x for x in receiver if x not in sender]))
-	print sender
 
 	while sender[-1] == receiver[len(sender) - 1]:
 		sender[-1] = choice([x for x in receiver if x not in sender])
@@ -15,4 +15,29 @@ for each in range(0, length):
 print "senders: " + str(sender)
 print "receivers: " + str(receiver)
 
-for x,y in sender
+# Launch server on the corresponding host.
+#  Note that sender - server, receiver - client.
+
+# fixed values for now. Iteration implementation to follow.
+server = 0
+client = 1
+
+# Start iperf on server/receiver host (non-blocking).
+serverCmd = "iperf -s"
+net.hosts[server].sendCmd(serverCmd)
+
+# Run multiple iperf runs on the client/sender.
+#   Run is repeated runCount times.
+#   Variable things:
+#     (-n)umber of bytes to transmit n[KM]
+payloadSize = "1K"
+runCount = 10
+for each in range(0,runCount):
+	clientCmd = "iperf -c " +  net.hosts[server].IP() \
+		+ " -n " + payloadSize + " -y c -x CSMV"
+
+	print "run #" + str(each)
+	print net.hosts[client].cmd(clientCmd)
+
+net.hosts[server].sendInt()
+net.hosts[server].monitor()
