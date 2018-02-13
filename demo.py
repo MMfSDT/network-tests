2cf8d941af7e53daf771de2a40ed8a0cd5821921
kylegomez <kyledominic.gomez@gmail.com>
2018-02-13 10:47:01 +0800

    code storage for possible reference

---
+++ demo.py
@@ -0,0 +1,22 @@
+
+print net.hosts[0] # can access different hosts through this/
+print net.hosts[0].cmd("ifconfig") # can be extended like so
+
+from pprint import pprint
+pprint(vars(net)) # show the attributes of an object
+
+from random import sample
+print sample(net.hosts, 2) # take two samples from a list
+
+# old code
+def pickHost(hosts):
+	from random import choice
+	listlen = len(hosts)
+
+	# choose receiver
+	receiver = choice(xrange(listlen))
+	while 
+		choice(xrange(listlen))
+
+	# choose sender
+	choice(xrange(listlen))
