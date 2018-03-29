15e21ac14602613d9bf7eca96d39861c1fbb89da
Charles Josh <cjoshua.alba@gmail.com>
2018-03-29 18:13:12 +0800

    Prep Juggler entry

--- clean_nohup.sh
+++ clean_nohup.sh
@@ -2,7 +2,7 @@
 
 sudo killall nohup
 sudo killall autorun.sh
-sudo killall /home/algoquin/behavioral-model/targets/simple_router/.libs/lt-simple_router
+sudo killall ../behavioral-model/targets/simple_router/.libs/lt-simple_router
 sudo killall run.sh
 sudo pkill -f topogen.py
 sudo pkill -f postprocess.py
