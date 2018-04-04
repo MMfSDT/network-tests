e9d42ef4cb32fa8ae251de5a9fc797216a40065b
kylegomez <kyledominic.gomez@gmail.com>
2018-04-04 18:54:50 +0800

    configuring manytoone onetomany tests

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from subprocess import Popen, PIPE
 from time import sleep
 import json
 
-testingInterval = "random"
+test = "onetomany"
 
 # Configuration
 ## network-tests and mininet-topo-generator should be in the same directory
@@ -55,88 +55,166 @@ if args['proto'] == "mptcp":
 ## Payload Size --payloadsize [(query),short,long]
 ### Addendum: Quarter size lonf due to test time
 ### Set maximum time for test
+# Note that these payloads are in MiB, KiB
 if args['payloadsize'] == "query":
 	payloadSize = "10K"
-	testingTime = 0.1
 elif args['payloadsize'] == "short":
 	payloadSize = "500K"
-	testingTime = 1
 elif args['payloadsize'] == "long": 
 	payloadSize = "25M"
-	testingTime = 100
 
 ## Run count --runcount [(10),N]
 runCount = int(args['runcount'])
 
-# Generate randomized sender/receiver pairs.
-## Each host will be placed into a fixed pair for the entirety of the tests.
+# Generate randomized sender/receiver arrays.
+## two hosts will be isolated in its own array, the rest will be in another
 length = len(net.hosts)
-clients = sample(xrange(length), length/2)
+clients = sample(xrange(length), 1)
 
-servers = []
-for each in range(0, length/2):
-	reserved = servers + clients
-	servers.append(choice([x for x in range(0, length) if x not in reserved]))
+servers = range(0, length)
+for host in clients:
+	servers.remove(host)
 
-print "*** Server-Client pairs"
-for server, client in zip(servers, clients):
-	print "*** " + str(net.hosts[server]) + "-" + str(net.hosts[client])
+print "*** Servers: "
+for server in servers:
+	print str(net.hosts[server])
+print "*** Clients: "
+for client in clients:
+	print str(net.hosts[client])
+
 print ""
 
 # We might have to log these down into another log file later on for parsing.
 ## Indicate the pairing, the time executed, and other pertinent details.
 
-if testingInterval != "random":
-	# Start iperf on all servers host (non-blocking).
-	serverCmd = "iperf -s &> /dev/null"
-	for host in servers:
-		net.hosts[host].sendCmd(serverCmd)
+if test == "onetomany":
+	# Transfer directories to network-tests/files where the payloads are 
+	print "*** changing directories"
+	cmd = "cd ../network-tests/files"
+	net.hosts[clients[0]].cmd(cmd)
 
-	print "*** starting iperf servers..."
-	print ""
+	# Start SimpleHTTPServer an all servers
+	print "*** starting servers"
+	for host in servers:
+		# Make sure all hosts in the servers list have changed directories
+		if net.hosts[host].cmd("pwd")[-5:] != "files":
+			cmd = "cd ../network-tests/files"
+			net.hosts[host].cmd(cmd)
+
+		cmd = "python -m SimpleHTTPServer &> /dev/null"
+		net.hosts[host].sendCmd(cmd)
+
 	sleep(1)
 
-	for client, server in zip(clients, servers):
-		# Append time into the log file
-		clientCmd = "iperf -c" + net.hosts[server].IP() + " -n " + "1" + " -y c -x CSMV" \
-		+ " >> ../network-tests/logs/tp/" + str(net.hosts[client]) + "-log"
-		net.hosts[client].sendCmd(clientCmd)
+	# Begin the file transfer
+	# Ip address, port, file for the wget command
+	print "*** sending requests"
+	for each in servers:
+		# cmd = "wget " + str(net.hosts[each].IP()) + ":8000/" + args['payloadsize'] + ".out" \
+		# 	" -P dump/" + args['payloadsize'] + "-" + str(each) + ".out"
+		
+		cmd = "wget " + str(net.hosts[each].IP()) + ":8000/" + args['payloadsize'] + ".out" + \
+			" -P dump/ &"
 
-else:
-	print "potato"
-	serverCmd = "iperf -s &> /dev/null"
+		print "Sending request to " + str(net.hosts[each])
+		net.hosts[clients[0]].cmd(cmd)
+
+	for host in clients:
+		net.hosts[host].monitor()
+
+	## Works until here.
+
 	for host in servers:
-		net.hosts[host].sendCmd(serverCmd)
+		print "*** stopping host " + str(net.hosts[host])
+		net.hosts[host].sendInt()
+		while net.hosts[host].waiting != False:
+			pass
 
-	print "*** starting iperf servers..."
-	print ""
-	sleep(1)
+		# net.hosts[host].monitor()	## does not terminate properly
 
-	endTime = time() + testingTime
-	clientQueue = clients
-	# Start the randomization process
-	while time() < endTime:
-		if len(clientQueue) != 0:
-			client = sample(clientQueue, 1)[0]
-			clientQueue.remove(client)
-		else:
-			break
+# elif test == "manytoone":
+# 	# Transfer directories to network-tests/files where the payloads are 
+# 	print "*** changing directories"
+# 	cmd = "cd ../network-tests/files"
+# 	net.hosts[clients[0]].cmd(cmd)
 
-		clientCmd = "iperf -c" + net.hosts[servers[clients.index(client)]].IP() + " -n " + "1" + " -y c -x CSMV" \
-		+ " >> ../network-tests/logs/tp/" + str(net.hosts[client]) + "-log"
-		net.hosts[client].sendCmd(clientCmd)
+# 	# Ensure that all the nodes have transfered directories
+# 	for host in servers:
+# 		if net.hosts[host].cmd("pwd")[-5:] != "files":
+# 			cmd = "cd ../network-tests/files"
+# 			net.hosts[host].cmd(cmd)
 
-		waitInterval = uniform(0, testingTime/len(clients))
-		sleep(waitInterval)
+# 	sleep(1)
 
-# Wait for the senders to finish before closing them
-for host in clients:
-	net.hosts[host].monitor()
+# 	for each in servers:
+# 		cmd = "wget " + str(net.hosts[clients[0]].IP()) + ":8000/" + args['payloadsize'] + ".out" + \
+# 			" -P dump/ &" 
 
-for host in servers:
-	print "*** stopping host " + str(net.hosts[host])
-	net.hosts[host].sendInt()
-	net.hosts[host].monitor()
+# 		print "Sending request from " + str(net.hosts[each])
+# 		net.hosts[each].cmd(cmd)
+
+# 	for host in clients:
+# 		print "*** waiting for sending"
+# 		net.hosts[host].monitor()
+
+# 	for host in servers:
+# 		print "*** stopping host " + str(net.hosts[host])
+# 		net.hosts[host].sendInt()
+# 		net.hosts[host].monitor()	## does not terminate properly
+
+# if testingInterval != "random":
+# 	# Start iperf on all servers host (non-blocking).
+# 	serverCmd = "iperf -s &> /dev/null"
+# 	for host in servers:
+# 		net.hosts[host].sendCmd(serverCmd)
+
+# 	print "*** starting iperf servers..."
+# 	print ""
+# 	sleep(1)
+
+# 	for client, server in zip(clients, servers):
+# 		# Append time into the log file
+# 		clientCmd = "iperf -c" + net.hosts[server].IP() + " -n " + "1" + " -y c -x CSMV" \
+# 		+ " >> ../network-tests/logs/tp/" + str(net.hosts[client]) + "-log"
+# 		net.hosts[client].sendCmd(clientCmd)
+
+# else:
+# 	print "potato"
+# 	serverCmd = "iperf -s &> /dev/null"
+# 	for host in servers:
+# 		net.hosts[host].sendCmd(serverCmd)
+
+# 	print "*** starting iperf servers..."
+# 	print ""
+# 	sleep(1)
+
+# 	endTime = time() + testingTime
+# 	clientQueue = clients
+# 	# Start the randomization process
+# 	while time() < endTime:
+# 		if len(clientQueue) != 0:
+# 			client = sample(clientQueue, 1)[0]
+# 			clientQueue.remove(client)
+# 		else:
+# 			break
+
+# 		clientCmd = "iperf -c" + net.hosts[servers[clients.index(client)]].IP() + " -n " + "1" + " -y c -x CSMV" \
+# 		+ " >> ../network-tests/logs/tp/" + str(net.hosts[client]) + "-log"
+# 		net.hosts[client].sendCmd(clientCmd)
+
+# 		waitInterval = uniform(0, testingTime/len(clients))
+# 		sleep(waitInterval)
+
+	# # Wait for the senders to finish before closing them
+	# for host in clients:
+	# 	net.hosts[host].monitor()
+
+	# for host in servers:
+	# 	print "*** stopping host " + str(net.hosts[host])
+	# 	net.hosts[host].sendInt()
+	# 	net.hosts[host].monitor()
+
+# Clear test files in dump
 
 # for server, client in zip(server, client):
 	
